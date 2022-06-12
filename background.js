d839d0a2a8c41ac5fd8649aec99b2d65db7ba9fb
Scott Hitchcock <shi83@uclive.ac.nz>
2022-06-13 10:33:58 +1200

    catch for if there are no images on the page

--- background.js
+++ background.js
@@ -2,23 +2,42 @@ const JOHNNY_DISPLAY_TIME_IN_MS = 600;
 
 JOHNNY_URL = "https://raw.githubusercontent.com/scotthitch/Johnny/main/images/johnny.png";
 
-const heresJohnny = () => {
-  // let imgs = document.querySelectorAll("img");
-  let images = document.images;
-
+const selectRandomImage = (images) => {
   const nImages = images.length;
+
+  // If there are no images return null
+  if (nImages === 0) {
+    return null;
+  }
+
+  // Select a random image and return it
   const indexToReplace = Math.floor(Math.random() * nImages);
-  let selectedImg = images[indexToReplace];
-  const originalImgURL = selectedImg.src;
-  selectedImg.src = JOHNNY_URL;
+  let selectedImage = images[indexToReplace];
+  return selectedImage;
+};
+
+const heresJohnny = () => {
+  // Get a list of all the images
+  let images = document.querySelectorAll("img");
+
+  // Select a random image
+  let selectedImage = selectRandomImage(images);
+
+  // If no valid images then exit out
+  if (selectedImage === null) {
+    return;
+  }
+
+  // Store the selected images url
+  const originalImgURL = selectedImage.src;
+
+  // Set the selected images url to that of Johnny
+  selectedImage.src = JOHNNY_URL;
 
   setTimeout(() => {
-    // Wait 20 seconds for cena to finish
-    selectedImg.src = originalImgURL;
+    // Wait 0.6s and then revert the image to its original url
+    selectedImage.src = originalImgURL;
   }, JOHNNY_DISPLAY_TIME_IN_MS);
 };
 
-// const chance = Math.floor(Math.random() * 2);
-// if (chance === 1) {
 heresJohnny();
-// }
